56317863133953aa1383dea2f079000fa5fcc8fb
Simo Piiroinen <simo.piiroinen@jollamobile.com>
2012-11-16 16:37:50 +0200

    Add context information for calibration data loading errors
    Instead of reporting "calibration" data" errors, mce now reports
    "ALS calibration" or "PS calibration" issues.

--- modules/filter-brightness-als.c
+++ modules/filter-brightness-als.c
@@ -421,14 +421,14 @@ static void calibrate_als(void)
 	/* Retrieve the calibration data from sysinfo */
 	if (get_sysinfo_value(ALS_CALIB_IDENTIFIER, &tmp, &len) == FALSE) {
 		mce_log(LL_ERR,
-			"Failed to retrieve calibration data");
+			"Failed to retrieve ALS calibration data");
 		goto EXIT;
 	}
 
 	/* Is the memory properly aligned? */
 	if ((len % sizeof (guint32)) != 0) {
 		mce_log(LL_ERR,
-			"Invalid calibration data returned");
+			"Invalid ALS calibration data returned");
 		goto EXIT2;
 	}
 
@@ -437,14 +437,14 @@ static void calibrate_als(void)
 	/* We don't have any calibration data */
 	if (count == 0) {
 		mce_log(LL_INFO,
-			"No calibration data available");
+			"No ALS calibration data available");
 		goto EXIT2;
 	}
 
 	switch (count) {
 	default:
 		mce_log(LL_INFO,
-			"Ignored excess calibration data");
+			"Ignored excess ALS calibration data");
 		/* Fall-through */
 
 	case 2:
