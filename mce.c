38bffceb3f9de557effb8a66680d3461d84913cd
Simo Piiroinen <simo.piiroinen@jollamobile.com>
2014-02-06 18:37:37 +0200

    Enable tklock if device lock gets activated
    Otherwise having device lock timeout that is shorter than tklock
    grace period can lead into situation where the device is locked but
    the last active application can still be made visible and used.
    
    [mce] Enable tklock if device lock gets activated

--- mce.c
+++ mce.c
@@ -1002,8 +1002,8 @@ int main(int argc, char **argv)
 		       0, GINT_TO_POINTER(FALSE));
 	setup_datapipe(&packagekit_locked_pipe, READ_ONLY, DONT_FREE_CACHE,
 		       0, GINT_TO_POINTER(FALSE));
-
-
+	setup_datapipe(&device_lock_active_pipe, READ_ONLY, DONT_FREE_CACHE,
+		       0, GINT_TO_POINTER(FALSE));
 
 	/* Initialise mode management
 	 * pre-requisite: mce_gconf_init()
@@ -1132,6 +1132,7 @@ EXIT:
 	free_datapipe(&heartbeat_pipe);
 	free_datapipe(&lipstick_available_pipe);
 	free_datapipe(&packagekit_locked_pipe);
+	free_datapipe(&device_lock_active_pipe);
 
 	/* Call the exit function for all subsystems */
 	mce_gconf_exit();
