352591e9c825e06de7c6d3b44757c89a85150418
Simo Piiroinen <simo.piiroinen@jollamobile.com>
2018-09-28 15:08:23 +0300

    [suspend] Expose suspend type within mce. JB#42611
    Enabler for dealing with subtle differences between earlysuspend and
    autosleep models that are not directly related to suspend policy.
    
    Signed-off-by: Simo Piiroinen <simo.piiroinen@jollamobile.com>

--- libwakelock.h
+++ libwakelock.h
@@ -13,6 +13,21 @@ extern "C" {
 };
 # endif
 
+/** Suspend model supported by the kernel */
+typedef enum {
+    /* Not known yet */
+    SUSPEND_TYPE_UNKN  = -1,
+
+    /* Suspend not supported */
+    SUSPEND_TYPE_NONE  =  0,
+
+    /* Early suspend model */
+    SUSPEND_TYPE_EARLY =  1,
+
+    /* Autosleep model */
+    SUSPEND_TYPE_AUTO  =  2,
+} suspend_type_t;
+
 void wakelock_lock  (const char *name, long long ns);
 void wakelock_unlock(const char *name);
 
@@ -21,6 +36,7 @@ void wakelock_block_suspend(void);
 void wakelock_block_suspend_until_exit(void);
 
 void lwl_enable_logging(void);
+suspend_type_t lwl_probe(void);
 
 # ifdef __cplusplus
 };
